a60bf1ad4ab5f78888760d09e9412d09fd9d6ac1
vickyruud <aprajit.k@gmail.com>
2022-07-18 16:29:45 -0400

    set up game start screen with text input and buttons

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,29 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+export default function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: "#72063c",
+    // elevation adds shadow to android
+    elevation: 4,
+    // shadow adds shadow to iOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
