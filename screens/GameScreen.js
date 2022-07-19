7a8030340c2aa997bbf3f18faef50334d2f7ca3f
vickyruud <aprajit.k@gmail.com>
2022-07-18 21:22:57 -0400

    start games screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
 export default function GameScreen() {
   return (
-    <View>
-      <Text>GameScreen</Text>
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <Text>Guess</Text>
+      <View>
+        <Text>Higher or Lower?</Text>
+      </View>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
